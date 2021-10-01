993bb97005e6956ae1d87d670032ed2f64429053
TOGHRUL SHAMSIZADE <91716304+ToghrulSh@users.noreply.github.com>
2021-10-01 06:02:47 +0000

    Implemented homework1

---
+++ main.py
@@ -0,0 +1,11 @@
+#Part 1---------------
+print('Hello World')
+#Part 2---------------
+import math
+
+x = int(input("Enter a number x: "))
+y = int(input("Enter a number y: "))
+
+print(x**y)
+print(math.log2(x))
+print("My student ID number: 77912")
